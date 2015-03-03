c7009e22d91c00745cf9274cdcf7e99e42526e72
Souleris <jgecys@gmail.com>
2015-03-03 13:57:50 +0200

    Dinaminio masyvo klase

--- GUIEssentials.cpp
+++ GUIEssentials.cpp
@@ -6,6 +6,8 @@ using namespace GUIEssentials;
 using namespace System;
 using namespace std;
 
+#pragma region Settings
+
 bool Settings::LoadFile(String^ file){
 	_isSaved = true;
 	_fileName = file;
@@ -70,7 +72,6 @@ bool Settings::SaveCurrent()
 	return true;
 }
 
-
 int Settings::TryGetInt(String^ key){
 	try{
 		int value = int::Parse((String^)_parameters[key]);
@@ -88,6 +89,7 @@ int Settings::TryGetInt(String^ key){
 	return 0;
 
 }
+
 float Settings::TryGetFloat(String^ key){
 	try{
 		float value = float::Parse((String^)_parameters[key]);
@@ -104,11 +106,10 @@ float Settings::TryGetFloat(String^ key){
 	}
 	return 0;
 }
+
 String^ Settings::TryGetString(String^ key){
 	return (String^) _parameters[key];
 }
-
-
 
 void Settings::SetValue(String^ key, String^ value){
 	_isSaved = false;
@@ -124,16 +125,17 @@ bool Settings::HasKey(String^ key){
 	return _parameters->ContainsKey(key);
 }
 
-
-
 ICollection^ Settings::GetKeys(){
 	return _parameters->Keys;
 }
+
 ICollection^ Settings::GetValues(){
 	return _parameters->Values;
 }
 
+#pragma endregion
 
+#pragma region MessageShow
 DialogResult MessageShow::Show(String^ Msg,
 	String^ Title,
 	MessageBoxButtons buttons,
@@ -155,3 +157,149 @@ DialogResult MessageShow::Show(String^ Msg,
 	MessageBoxButtons buttons){
 	return MessageShow::Show(Msg, Title, buttons, MessageBoxIcon::None);
 }
+#pragma endregion
+
+#pragma region DinaminisMasyvas
+
+GUIEssentials::DinaminisMasyvas::DinaminisMasyvas();
+GUIEssentials::DinaminisMasyvas::DinaminisMasyvas(Object O[], size_type n);
+GUIEssentials::DinaminisMasyvas::DinaminisMasyvas(const DinaminisMasyvas & other);
+GUIEssentials::DinaminisMasyvas::~DinaminisMasyvas();
+
+template<class Object>
+void GUIEssentials::DinaminisMasyvas<Object>::Isvalyti(){
+	n = nMax = 0;
+	if (Masyvas)
+		delete[] Masyvas;
+	Masyvas = NULL;
+}
+
+
+template<class Object>
+void GUIEssentials::DinaminisMasyvas<Object>::KeistiDydi(size_type dydis){
+	if (dydis > nMax){
+		Object * naujasMasyvas = new Object[dydis];
+		for (size_tyoe i = 0; i < n; i++){
+			naujasMasyvas[i] = Masyvas[i];
+		}
+		delete[] Masyvas;
+		Masyvas = naujasMasyvas;
+	}
+	else{
+		Object * naujasMasyvas = new Object[dydis];
+		for (size_tyoe i = 0; i < dydis; i++){
+			naujasMasyvas[i] = Masyvas[i];
+		}
+		delete[] Masyvas;
+		Masyvas = naujasMasyvas;
+		if (n > dydis){
+			n = dydis;
+		}
+	}
+	nMax = dydis;
+}
+
+
+template<class Object>
+Object * GUIEssentials::DinaminisMasyvas<Object>::Pradzia(){
+	if (n == 0){
+		return NULL;
+	}
+	Object * rodykle;
+	rodykle = &Masyvas[0];
+	return rodykle;
+}
+
+template<class Object>
+Object * GUIEssentials::DinaminisMasyvas<Object>::Pabaiga(){
+	if (n == 0){
+		return NULL;
+	}
+	Object * rodykle;
+	rodykle = &Masyvas[n - 1];
+	return rodykle;
+}
+
+template<class Object>
+size_type GUIEssentials::DinaminisMasyvas<Object>::ImtiN() const{
+	return n;
+}
+
+
+template<class Object>
+Object GUIEssentials::DinaminisMasyvas<Object>::Imti(size_type i) const{
+	return Masyvas[i];
+}
+
+template<class Object>
+void GUIEssentials::DinaminisMasyvas<Object>::Deti(Object obj){
+	if (n == nMax) KeistiDydi(nMax + Cd);
+	Masyvas[n++] = obj;
+}
+
+template<class Object>
+void GUIEssentials::DinaminisMasyvas<Object>::Iterpti(size_type i, Object obj){
+	if (n == nMax) KeistiDydi(nMax + Cd);
+	if (n == 0) return;
+	for (size_type j = n; j >= i; j--){
+		Masyvas[j + 1] = Masyvas[j];
+	}
+	Masyvas[i] = obj;
+}
+
+template<class Object>
+void GUIEssentials::DinaminisMasyvas<Object>::Istrynti(size_type i){
+	if (n == 1){
+		Isvalyti();
+		return;
+	}
+	if (n - nMax < Cd) KeistiDydi(nMax - Cd);
+	if (n == 0) return;
+	n--;
+	for (size_type j = i; j < n; j++){
+		Masyvas[j] = Masyvas[j + 1];
+	}
+}
+
+template<class Object>
+void GUIEssentials::DinaminisMasyvas<Object>::Istrynti(Object obj){
+	size_type i = RastiOjektoIndeksa(obj);
+	if ( i > -1 )
+		Istrynti(i);
+}
+
+template<class Object>
+Object & GUIEssentials::DinaminisMasyvas<Object>::operator [] (const size_type & i){
+	return Masyvas[i];
+}
+
+template<class Object>
+DinaminisMasyvas<Object> & GUIEssentials::DinaminisMasyvas<Object>::operator = (const DinaminisMasyvas<Object> & other){
+	Isvalyti();
+	n = other.n;
+	nMax = other.nMax;
+	Masyvas = new Object[nMax];
+	for (size_type i = 0; i < n; i++){
+		Masyvas[i] = other.Masyvas[i];
+	}
+	return *this;
+}
+
+template<class Object>
+DinaminisMasyvas GUIEssentials::DinaminisMasyvas<Object>::operator + (const DinaminisMasyvas<Object> & other){
+	size_type Dydis = nMax + other.nMax
+	Object* naujas = new Object[Dydis];
+	for (size_type i = 0; i < n; i++){
+		naujas[i] = Masyvas[i];
+	}
+
+	for (size_type i = 0; i < other.n; i++){
+		naujas[n + i] = other.Masyvas[i];
+	}
+
+	DinaminisMasyvas<Object> Mas(naujas, Dydis);
+	return Mas;
+}
+
+
+#pragma endregion
--- GUIEssentials.h
+++ GUIEssentials.h
@@ -10,6 +10,166 @@ using namespace std;
 
 namespace GUIEssentials {
 
+	template<class Object>
+	public class DinaminisMasyvas{
+	public:
+		/**
+		 * @typedef	unsigned int size_type
+		 *
+		 * @brief	Dinaminio masyvo dydzio tipas
+		 */
+		typedef unsigned int size_type;
+		/** @brief	Dinaminis masyvas didinamas po Cd elementu. */
+		const static size_type Cd = 10;
+	private:
+		/**
+		 * @brief	Randa ojekto indeksa.
+		 *
+		 * @param	obj	Objektas, kurio indekso iesko.
+		 *
+		 * @return	Objekto indexas.
+		 */
+		size_type RastiOjektoIndeksa(const Object & obj);
+	protected:
+		/** @brief	Dinaminis masyvas. */
+		Object *Masyvas;
+		/** @brief	Dinaminio masyvo dydziai. */
+		size_type n, nMax;
+	public:
+
+		/**
+		 * @brief	Tuscias Konstruktorius.
+		 */
+		DinaminisMasyvas();
+
+		/**
+		 * @fn	DinaminisMasyvas::DinaminisMasyvas(Object *O, size_type n);
+		 *
+		 * @brief	Konstruktorius.
+		 *
+		 * @param [in,out]	O	Jei ne NULL, sukuria Objekta su siuo dinaminiu masyvu.
+		 * @param	n		 	Dinaminio masyvo dydis.
+		 */
+
+		DinaminisMasyvas(Object *O, size_type n);
+
+		/**
+		 * @brief	Kopijavimo konstruktorius.
+		 *
+		 * @param	other	Objektas, kuri kopijuoja.
+		 */
+
+		DinaminisMasyvas(const DinaminisMasyvas & other);
+
+		/**
+		 * @brief	Destruktorius.
+		 */
+
+		~DinaminisMasyvas();
+
+		/**
+		 * @brief	Atlaisvina dinaminio masyvo atminti.
+		 */
+		void Isvalyti();
+		/**
+		 * @brief	Padidina/sumazina dinamini masyva.
+		 *
+		 * @param	dydis	Dinaminio masyvo dydis.
+		 */
+		void KeistiDydi(size_type dydis);
+
+		/**
+		 * @brief	Grazina rodykle i pirma masyvo elementa.
+		 *
+		 * @return	null jei tuscia, kitu atveju pirmas masyvo objektas.
+		 */
+		Object * Pradzia();
+		/**
+		 * @brief	Grazina rodykle i paskutini masyvo elementa.
+		 *
+		 * @return	null jei tuscia, kitu atveju paskutinis masyvo objektas.
+		 */
+		Object * Pabaiga();
+
+		/**
+		 * @return	Dinaminio masyvo dydi..
+		 */
+		size_type ImtiN() const;
+
+		/**
+		 * @brief	Grazina i-taji elementa;
+		 *
+		 * @param	i	Elemento indeksas.
+		 *
+		 * @return	i-taji elementa.
+		 */
+
+		Object Imti(size_type i) const;
+
+		/**
+		 * @brief	Prideda nauja objekta
+		 *
+		 * @param	obj	Objektas, kuri ideda.
+		 */
+
+		void Deti(Object obj);
+
+		/**
+		 * @brief	Iterptia objekta i dinamini masyva.
+		 *
+		 * @param	i  	I kuria vieta iterpiamos objektas.
+		 * @param	obj	Iterpiamas objektas.
+		 */
+
+		void Iterpti(size_type i, Object obj);
+
+		/**
+		 * @brief	Istrina objekta i pozicijoje
+		 *
+		 * @param	i	Objekto pozicija
+		 */
+
+		void Istrynti(size_type i);
+
+		/**\
+		 * @brief	Istrina objekta
+		 *
+		 * @param	obj Objektas.
+		 */
+
+		void Istrynti(Object obj);
+
+		/**
+		 * @brief	Masyvo operatorius
+		 *
+		 * @param	i	Indeksas
+		 *
+		 * @return	The indexed value.
+		 */
+
+		Object & operator [] (const size_type & i);
+
+		/**
+		 * @brief	Priskyrimo operatorius
+		 *
+		 * @param	other	Kitas objektas.
+		 *
+		 * @return	Objekto kopija
+		 */
+
+		DinaminisMasyvas<Object> & operator = (const DinaminisMasyvas<Object> & other);
+
+		/**
+		 * @brief	Sujungia 2  to pacio tipo dinaminius masyvus
+		 *
+		 * @param	other	Kitas dinaminis masyvas
+		 *
+		 * @return	Sujungtas masyvas.
+		 */
+
+		DinaminisMasyvas operator + (const DinaminisMasyvas<Object> & other);
+	}
+
 	public ref class Settings
 	{
 	private:
